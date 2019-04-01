f2a9953a745dc2b38e0b0c4100426a55c10718d3
Ay Has <ahashim983@yahoo.com>
2019-04-01 13:12:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,44 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  k = languages[:oo].keys.concat(languages[:functional].keys).uniq
+  for item in k
+    puts languages[:oo][item][:type]
+    puts languages[:functional][item][:type]
+  end
+
 end
+
+puts reformat_languages(languages)
